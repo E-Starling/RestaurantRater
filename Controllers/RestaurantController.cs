2302d93fa5449d8a8e6c1be54dfe248f3bb111f4
Ethan Starling <ethan.jr.starling@gmail.com>
2022-04-25 12:19:58 -0400

    added more to restaurant controller

--- Controllers/RestaurantController.cs
+++ Controllers/RestaurantController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using RestaurantRater.Models;
 
 namespace RestaurantRater.Controllers
 {
     public class RestaurantController : Controller
     {
+        private RestaurantDbContext _db = new RestaurantDbContext();
+
         // GET: Restaurant/Index
         public ActionResult Index()
         {
-            return View();
+            return View(_db.Restaurants.ToList());
         }
     }
 }
